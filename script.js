110a4b914d16a2b01d78b41455d4622c3d116ed7
dan <realdanquirion@gmail.com>
2023-10-06 15:51:19 -0400

    Declared the variables for local storage correctly so now the application is working

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector('#addTodo');
     const input = document.querySelector('#todo');
     const list = document.querySelector('#todoList');
-    
-const savedTodo = JSON.parse(localStorage.getItem("todos")) || [];
-for (let i = 0; i < savedTodo.length; i++){
-    let newTodo = document.createElement("li");
-  newTodo.innerText = savedTodos[i].task;
-  newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
-  if (newTodo.isCompleted) {
-    newTodo.style.textDecoration = "line-through";
-}
-list.appendChild(newTodo);
-}
-    list.addEventListener('click', function(e) {
+
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    for (let i = 0; i < savedTodos.length; i++) {
+        let newTodo = document.createElement("li");
+        newTodo.innerText = savedTodos[i].task;
+        newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+        if (newTodo.isCompleted) {
+            newTodo.style.textDecoration = "line-through";
+        }
+        list.appendChild(newTodo);
+    }
+    list.addEventListener('click', function (e) {
         if (e.target.tagName === 'BUTTON') {
             e.target.parentElement.remove();
         } else if (e.target.tagName === 'LI') {
             e.target.classList.add('done');
-            
+
         }
         for (let i = 0; i < savedTodos.length; i++) {
             if (savedTodos[i].task === clickedListItem.innerText) {
-              savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
-              localStorage.setItem("todos", JSON.stringify(savedTodos));
+                savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
+                localStorage.setItem("todos", JSON.stringify(savedTodos));
             }
-          }
+        }
     });
-    
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         const newTodo = document.createElement('li');
-        const removeButton  = document.createElement('button');
+        const removeButton = document.createElement('button');
         removeButton.innerText = 'Remove';
         newTodo.innerText = input.value;
         newTodo.appendChild(removeButton);
